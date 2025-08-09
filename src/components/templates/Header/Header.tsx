060e731a3bea57d8a53881a05c0e59eba37c1f26
Dominique Talis <dominique.talis@proton.me>
2025-08-08 20:26:11 -0400

    feat(nav): add 'key' to items

--- src/components/templates/Header/Header.tsx
+++ src/components/templates/Header/Header.tsx
@@ -27,7 +27,7 @@ export const Header = () => {
 			>
 				<div className="flex lg:flex-1">
 					<a
-						href="#"
+						href="/"
 						className="-m-1.5 p-1.5"
 					>
 						<span className="sr-only">Ooneex</span>
@@ -65,6 +65,7 @@ export const Header = () => {
 				<div className="hidden lg:flex lg:gap-4 lg:flex-1 lg:justify-end">
 					{signin.map((item) => (
 						<a
+							key={item.name}
 							href={item.href}
 							className={`${
 								item.name === "Signup" ? "text-secondary " : ""
@@ -96,7 +97,7 @@ export const Header = () => {
 				<DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 					<div className="flex items-center justify-between">
 						<a
-							href="#"
+							href="/"
 							className="-m-1.5 p-1.5"
 						>
 							<span className="sr-only">Ooneex</span>
@@ -134,8 +135,11 @@ export const Header = () => {
 							<div className="py-6">
 								{signin.map((item) => (
 									<a
+										key={item.name}
 										href={item.href}
-										className={`${item.name === "Signup" ? "text-secondary " : ""}-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50`}
+										className={`${
+											item.name === "Signup" ? "text-secondary " : ""
+										}-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50`}
 									>
 										{item.name}
 									</a>
